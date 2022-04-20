37770b92381bfabf88f69b1ce4c219a20d064028
Sean Yang <seanyang0506@gmail.com>
2022-04-19 21:19:18 -0700

    center, container

--- tutorial/lib/main.dart
+++ tutorial/lib/main.dart
@@ -21,37 +21,38 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Awesome App"),
+        title: const Text("Awesome App"),
       ),
       body: Center(
         child: Container(
-          padding: const EdgeInsets.all(8),
-          width: 100,
           height: 100,
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            color: Colors.red,
-            borderRadius: BorderRadius.circular(10),
-            gradient: const LinearGradient( colors: [
-              Colors.pink,
-              Colors.yellow,
-            ]),
-            boxShadow: const [
-              BoxShadow(
-                color: Colors.grey,
-                blurRadius: 10,
-              )
-            ]
-          ),
-          child: const Text(
-            "I am a box",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-
-            ),
+          color: Colors.teal,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.all(8),
+                width: 100,
+                height: 100,
+                color: Colors.red,
+                alignment: Alignment.center,
+              ),
+              Container(
+                padding: const EdgeInsets.all(8),
+                width: 100,
+                height: 100,
+                color: Colors.yellow,
+                alignment: Alignment.center,
+              ),
+              Container(
+                padding: const EdgeInsets.all(8),
+                width: 100,
+                height: 100,
+                color: Colors.green,
+                alignment: Alignment.center,
+              ),
+            ],
           ),
         ),
       ),
